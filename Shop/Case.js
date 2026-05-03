d1b6e4cdd9909973d121747c554cab59c4a303cc
Jolian Mars L. Dela Cruz <jolianmars123@gmail.com>
2026-05-04 07:57:57 +0800

    feat: Add OpenCase page and move logic to OpenCaseGun

---
+++ Shop/Case.js
@@ -0,0 +1,11 @@
+class Case{
+    constructor(name,items,price,caseType){
+        this.name = name;
+        this.items = items;
+        this.price = price;
+        this.caseType = caseType;
+    }
+    
+//Wait lemme think..
+
+}
